c7eafcdafe2027799e4d2cce293f382e5da165e2
agustinguerra <agustin.guerraguerra8@gmail.com>
2017-09-07 16:57:07 -0300

    Avan

---
+++ Entrega2/HashAbierto.h
@@ -0,0 +1,60 @@
+#pragma once
+
+
+template <class D, class R>
+class HashAbierto : public Table<D, R>
+{
+public:
+	~HashAbierto() { }
+
+	/* CONSTRUCTORAS */
+
+	//PRE: T(c) no está definida y la tabla no está llena
+	//POS: Define T(c) = v
+	void Agregar(const C& c, const V& v) override;
+
+	//PRE: T(c) está definida
+	//POS: Borra la asociación ligada a 'c'
+	void Borrar(const C& c) override;
+
+	//PRE: - 
+	//POS: Borra todas las asociaciones
+	void BorrarTodos() override;
+
+	/* PREDICADOS */
+
+	//PRE: - 
+	//POS: Retorna true si está vacía, false sino.
+	bool EstaVacia() const override;
+
+	//PRE: - 
+	//POS: Retorna true si está llena, false sino.
+	bool EstaLlena() const override;
+
+	//PRE: - 
+	//POS: Retorna true si T(c) está definida, es decir, si la clave c está definida. False sino.
+	bool EstaDefinida(const C& c) const override;
+
+	/* SELECTORAS */
+
+	//PRE: T(c) está definida
+	//POS: Retorna 'v', tal que T(c) = v
+	const V& Obtener(const C& c) const override;
+
+	//PRE: -
+	//POS: Retorna el largo de la tabla
+	nat Largo() const override;
+
+	//PRE: -
+	//POS: Devuelve un clon de la tabla, no comparten memoria
+	Puntero<Tabla<C, V>> Clonar() const override;
+
+	Iterador<T> ObtenerIterador() const override;
+
+private:
+	Array<Puntero<Lista<Asociacion<D, R>>>> laTabla;
+	Puntero<FunctionHash<Asociacion<D, R>>> pFUnc;
+
+};
+
+#include "HashAbierto.cpp"
